16001512de8a4ea38d8889f407f782a54cce39da
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2025-10-02 20:00:54 +0100

    Added module tests for events and users module

---
+++ ModularMonolith/Modules/Events/Testing.Architecture.Events/Modules/Module.steps.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+using Application.Events;
+using BDD;
+using Domain.Events.Entities;
+using NetArchTest.Rules;
+
+namespace Testing.Architecture.Events.Modules;
+
+[TestFixture]
+internal partial class ModuleSpecs : Specification
+{
+    private static Assembly DomainAssembly => typeof(Event).Assembly;
+    private static Assembly ApplicationAssembly => EventsIntegrationMessaging.Assembly;
+    private static Assembly InfrastructureAssembly => typeof(Infrastructure.Events.Persistence.EventRepository).Assembly;
+    private static Assembly ControllerAssembly => typeof(Controllers.Events.EventController).Assembly;
+    private TestResult testResult;
+    
+    protected override void before_each()
+    {
+        base.before_each();
+        testResult = null!;
+    }
+    
+    private void checking_the_domain_layer_for_application_layer_references()
+    {
+        testResult = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
+            .GetResult();
+    }
+
+    private void checking_the_application_layer_for_infrastructure_layer_references()
+    {
+        testResult = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+    }    
+    
+    private void checking_the_infrastructure_layer_for_controller_layer_references()
+    {
+        testResult = Types.InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(ControllerAssembly.GetName().Name)
+            .GetResult();
+    }
+
+    private void there_should_be_no_references_to_infrastructure_layer()
+    {
+        Assert.That(testResult.FailingTypes, Is.Null.Or.Empty);
+    }
+
+    private void there_should_be_no_references_to_application_layer()
+    {
+        Assert.That(testResult.FailingTypes, Is.Null.Or.Empty);
+    }
+
+    private void there_should_be_no_references_to_controller_layer()
+    {
+        Assert.That(testResult.FailingTypes, Is.Null.Or.Empty);
+    }
+}

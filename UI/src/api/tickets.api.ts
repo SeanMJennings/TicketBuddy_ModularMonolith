e07231738d9e53bf156befc2e15817438178eb1d
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2025-11-10 16:13:56 +0000

    refactor

--- UI/src/api/tickets.api.ts
+++ UI/src/api/tickets.api.ts
@@ -10,14 +10,14 @@ export type TicketsPayload = {
     TicketIds: string[];
 };
 
-export const purchaseTickets = async (eventId: string, payload: TicketsPayload, jwt: string) => {
+export const purchaseTickets = async (eventId: string, payload: TicketsPayload, jwt: string | null | undefined) => {
     return post(`/events/${eventId}/tickets/purchase`, payload, jwt);
 }
 
-export const reserveTickets = async (eventId: string, payload: TicketsPayload, jwt: string) => {
+export const reserveTickets = async (eventId: string, payload: TicketsPayload, jwt: string | null | undefined) => {
     return post(`/events/${eventId}/tickets/reserve`, payload, jwt);
 }
 
-export const getTicketsForUser = async (userId: string, jwt: string) => {
+export const getTicketsForUser = async (userId: string, jwt: string | null | undefined) => {
     return get<Ticket[]>(`/tickets/users/${userId}`, jwt);
 }

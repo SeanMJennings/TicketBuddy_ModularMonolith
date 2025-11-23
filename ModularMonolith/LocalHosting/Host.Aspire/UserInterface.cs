94f0c16c1b9eb787a733ac4c74bb55e3ad654dda
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2025-11-23 19:10:07 +0000

    refactor

--- ModularMonolith/LocalHosting/Host.Aspire/UserInterface.cs
+++ ModularMonolith/LocalHosting/Host.Aspire/UserInterface.cs
@@ -1,8 +1,10 @@
-﻿namespace TicketBuddy.AppHost;
+﻿using System.Diagnostics;
+
+namespace TicketBuddy.AppHost;
 
 public static class UserInterface
 {
-    public const string ImageName = "localhosting-ui:local";
+    public const string ImageName = "ticketbuddy-ui:local";
     public static async Task CreateImage()
     {
         var repoRoot = FindRepoRootContaining(AppContext.BaseDirectory, "UI");
@@ -12,23 +14,31 @@ public static class UserInterface
         if (!Directory.Exists(uiBuildContext)) throw new DirectoryNotFoundException($"Could not locate folder `UI` at `{uiBuildContext}`");
         if (!File.Exists(uiDockerfile)) throw new FileNotFoundException($"Dockerfile not found at `{uiDockerfile}`");
 
-        var psi = new System.Diagnostics.ProcessStartInfo("docker",
-            $"build -f \"{uiDockerfile}\" -t {ImageName} \"{uiBuildContext}\"")
+        await RunDockerCommandAsync($"image rm -f {ImageName}", throwOnError: false);
+
+        var buildArgs = $"build -f \"{uiDockerfile}\" -t {ImageName} \"{uiBuildContext}\"";
+        await RunDockerCommandAsync(buildArgs, throwOnError: true);
+    }
+    
+    private static async Task RunDockerCommandAsync(string args, bool throwOnError)
+    {
+        var psi = new ProcessStartInfo("docker", args)
         {
             RedirectStandardOutput = true,
             RedirectStandardError = true,
             UseShellExecute = false
         };
 
-        using var proc = System.Diagnostics.Process.Start(psi)!;
-        var stdout = proc.StandardOutput.ReadToEndAsync();
-        var stderr = proc.StandardError.ReadToEndAsync();
+        using var proc = Process.Start(psi)!;
+        var stdoutTask = proc.StandardOutput.ReadToEndAsync();
+        var stderrTask = proc.StandardError.ReadToEndAsync();
         await proc.WaitForExitAsync();
-        if (proc.ExitCode != 0)
+        var outText = await stdoutTask;
+        var errText = await stderrTask;
+
+        if (proc.ExitCode != 0 && throwOnError)
         {
-            var outText = await stdout;
-            var errText = await stderr;
-            throw new InvalidOperationException($"Docker build failed for `UI`.\nStdout:\n{outText}\nStderr:\n{errText}");
+            throw new InvalidOperationException($"Docker command `docker {args}` failed.\nStdout:\n{outText}\nStderr:\n{errText}");
         }
     }
     

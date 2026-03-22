14ebfc37576fe30ad7e75c86d316775e5651a765
SeanJennings <seanjennings976@gmail.com>
2026-03-22 13:11:51 +0000

    increase coverage

---
+++ ModularMonolith/Modules/Tickets/Testing.Unit.Tickets/User.specs.cs
@@ -0,0 +1,86 @@
+using NUnit.Framework;
+
+namespace Unit;
+
+public partial class UserSpecs
+{
+    [Test]
+    public void can_create_a_user()
+    {
+        Given(valid_user_inputs);
+        When(creating_a_user);
+        Then(the_user_is_created);
+    }
+
+    [Test]
+    public void a_users_name_must_be_alphabetical()
+    {
+        Scenario(() =>
+        {
+            Given(valid_user_inputs);
+            And(a_null_name);
+            When(Validating(creating_a_user));
+            Then(Informs("Name cannot be null or empty"));
+        });
+
+        Scenario(() =>
+        {
+            Given(valid_user_inputs);
+            And(an_empty_name);
+            When(Validating(creating_a_user));
+            Then(Informs("Name cannot be null or empty"));
+        });
+
+        Scenario(() =>
+        {
+            Given(valid_user_inputs);
+            And(a_name_with_invalid_characters);
+            When(Validating(creating_a_user));
+            Then(Informs("Name can only have alphabetical characters"));
+        });
+    }
+
+    [Test]
+    public void a_users_email_must_be_valid()
+    {
+        Scenario(() =>
+        {
+            Given(valid_user_inputs);
+            And(a_null_email);
+            When(Validating(creating_a_user));
+            Then(Informs("Email cannot be null or empty"));
+        });
+
+        Scenario(() =>
+        {
+            Given(valid_user_inputs);
+            And(an_empty_email);
+            When(Validating(creating_a_user));
+            Then(Informs("Email cannot be null or empty"));
+        });
+
+        Scenario(() =>
+        {
+            Given(valid_user_inputs);
+            And(an_invalid_email);
+            When(Validating(creating_a_user));
+            Then(Informs("Email must be valid"));
+        });
+    }
+
+    [Test]
+    public void can_update_users_name()
+    {
+        Given(a_valid_user);
+        When(updating_user_name);
+        Then(user_name_is_updated);
+    }
+
+    [Test]
+    public void can_update_users_email()
+    {
+        Given(a_valid_user);
+        When(updating_user_email);
+        Then(user_email_is_updated);
+    }
+}
---
+++ ModularMonolith/Modules/Tickets/Testing.Unit.Tickets/User.steps.cs
@@ -0,0 +1,87 @@
+using BDD;
+using Domain.Tickets.User;
+using Shouldly;
+
+namespace Unit;
+
+public partial class UserSpecs : Specification
+{
+    private Guid id;
+    private string fullName = null!;
+    private string email = null!;
+    private User theUser = null!;
+
+    private const string valid_full_name = "John Smith";
+    private const string invalid_full_name = "John Smith 123!";
+    private const string updated_full_name = "Jane Doe";
+    private const string valid_email = "john@example.com";
+    private const string invalid_email = "not-an-email";
+    private const string updated_email = "jane@example.com";
+
+    protected override void before_each()
+    {
+        base.before_each();
+        id = Guid.CreateVersion7();
+        fullName = null!;
+        email = null!;
+        theUser = null!;
+    }
+
+    private void valid_user_inputs()
+    {
+        fullName = valid_full_name;
+        email = valid_email;
+    }
+
+    private void a_null_name() => fullName = null!;
+    private void an_empty_name() => fullName = string.Empty;
+    private void a_name_with_invalid_characters() => fullName = invalid_full_name;
+    private void a_null_email() => email = null!;
+    private void an_empty_email() => email = string.Empty;
+    private void an_invalid_email() => email = invalid_email;
+
+    private void a_valid_user()
+    {
+        valid_user_inputs();
+        creating_a_user();
+    }
+
+    private void creating_a_user()
+    {
+        theUser = new User(id, new Name(fullName), new Email(email));
+    }
+
+    private void updating_user_name()
+    {
+        theUser.UpdateName(new Name(updated_full_name));
+    }
+
+    private void updating_user_email()
+    {
+        theUser.UpdateEmail(new Email(updated_email));
+    }
+
+    private void the_user_is_created()
+    {
+        theUser.ShouldNotBeNull();
+        theUser.Id.ShouldBe(id);
+        (theUser.FullName == valid_full_name).ShouldBeTrue();
+        (theUser.FullName != valid_full_name).ShouldBeFalse();
+        (theUser.Email == valid_email).ShouldBeTrue();
+        (theUser.Email != valid_email).ShouldBeFalse();
+    }
+
+    private void user_name_is_updated()
+    {
+        string updatedName = theUser.FullName;
+        updatedName.ShouldBe(updated_full_name);
+        (theUser.FullName != valid_full_name).ShouldBeTrue();
+    }
+
+    private void user_email_is_updated()
+    {
+        string updatedEmailStr = theUser.Email;
+        updatedEmailStr.ShouldBe(updated_email);
+        (theUser.Email != valid_email).ShouldBeTrue();
+    }
+}

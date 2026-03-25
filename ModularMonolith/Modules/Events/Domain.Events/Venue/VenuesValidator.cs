6c4ac0478a64a082b82101bf5a6e46a9b3a27215
SeanJennings <seanjennings976@gmail.com>
2026-03-25 20:31:01 +0000

    increase coverage

--- ModularMonolith/Modules/Events/Domain.Events/Venue/VenuesValidator.cs
+++ ModularMonolith/Modules/Events/Domain.Events/Venue/VenuesValidator.cs
@@ -11,7 +11,7 @@ public static class VenuesValidator
     public static void CheckAddressUniqueness(Address address, IEnumerable<Venue> venues, Guid? excludeVenueId = null)
     {
         var existingVenue = venues
-            .Where(v => excludeVenueId == null || v.Id != excludeVenueId)
+            .Where(v => excludeVenueId == null || v.Id != excludeVenueId.Value)
             .FirstOrDefault(v => v.Address.Equals(address));
 
         if (existingVenue is not null) throw new ValidationException("A venue already exists at this address");
